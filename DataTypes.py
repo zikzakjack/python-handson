84c5319c3b487d356ab51d48959a89d86bccd66a
zikzakjack <kalyansmiles@gmail.com>
2022-06-26 08:56:16 +0530

    Python DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,61 @@
+################################################################################
+# Basic DataTypes
+# integer / decimal / boolean
+################################################################################
+
+# Integer
+var = 10
+print(var)
+print(type(var))
+
+# Decimal
+var = 20.0
+print(var)
+print(type(var))
+
+# Boolean
+var = 1 < 2
+print(var)
+print(type(var))
+
+################################################################################
+# Advanced DataTypes
+# Sequence Types - list, tuple, range, str
+# Set Types - Set, FrozenSet
+# Mapping Types - dict
+################################################################################
+
+# list
+var = [1, 2, 3, 4]
+print(var)
+print(type(var))
+
+# tuple
+var = (1, 2, 3, 4)
+print(var)
+print(type(var))
+
+# range
+var = range(0, 5)
+print(var)
+print(type(var))
+
+# String
+var = "Hello World"
+print(var)
+print(type(var))
+
+# set
+var = {"milk", "bread", "eggs"}
+print(var)
+print(type(var))
+
+# frozenset
+var = frozenset({"milk", "bread", "eggs"})
+print(var)
+print(type(var))
+
+# dict
+var = {1: 'a', 2: 'b', 3: 'c'}
+print(var)
+print(type(var))
